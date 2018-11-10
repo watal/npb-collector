ed409c8db9ef06f3f1d96ee58faf5efa83287bcc
watal <watal.i27e@gmail.com>
2018-11-10 14:20:37 +0900

    Comply with PEP8

--- npb-collector.py
+++ npb-collector.py
@@ -7,9 +7,10 @@ import multiprocessing as mp
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen
 
+
 def scraping_npb():
     # チームの判別子を格納
-    key_list = ["h","l","e","bs","f","m","c","t","db","g","d","s"]
+    key_list = ["h", "l", "e", "bs", "f", "m", "c", "t", "db", "g", "d", "s"]
     # 選手情報を格納するリスト
     players_list = []
 
@@ -19,7 +20,7 @@ def scraping_npb():
         # ページ情報を取得
         team_page = bs(urlopen(team_url), 'lxml')
         # ページ内の選手情報を取得
-        players = team_page.find_all("a",class_='player_unit_1')
+        players = team_page.find_all("a", class_='player_unit_1')
 
         for player in players:
             # 選手情報から個人ページのURLを取得
@@ -45,19 +46,22 @@ def scraping_npb():
                     career = table.find_all('td')[4].text.strip('\n')
                     draft = table.find_all('td')[5].text.strip('\n')
 
-                    tmp_list = [{"team":div.find('li', id='pc_v_team').text.strip('\n')}]       # チーム名
-                    tmp_list.append({"name":" ".join(name.split())})                            # 名前
-                    tmp_list.append({"position":position.strip()})                              # ポジション
-                    tmp_list.append({"toda":toda.strip()})                                      # 投打
-                    tmp_list.append({"height_weight":height_weight.strip()})                    # 身長体重
-                    tmp_list.append({"born":born.strip()})                                      # 生年月日
-                    tmp_list.append({"career":career.strip()})                                  # 経歴
-                    tmp_list.append({"draft":draft.strip()})                                    # ドラフト
+                    tmp_list = [
+                        {"team": div.find('li', id='pc_v_team').text.strip('\n')}]  # チーム名
+                    tmp_list.append({"name": " ".join(name.split())})   # 名前
+                    tmp_list.append({"position": position.strip()})     # ポジション
+                    tmp_list.append({"toda": toda.strip()})             # 投打
+                    tmp_list.append(
+                        {"height_weight": height_weight.strip()})       # 身長体重
+                    tmp_list.append({"born": born.strip()})             # 生年月日
+                    tmp_list.append({"career": career.strip()})         # 経歴
+                    tmp_list.append({"draft": draft.strip()})           # ドラフト
                     players_list.append(tmp_list)
 
     # ファイル出力
-    rslt_file = open('data/baseball_player'  + '.json', 'w')
+    rslt_file = open('data/baseball_player' + '.json', 'w')
     json.dump(players_list, rslt_file, ensure_ascii=False, indent=2)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     scraping_npb()
